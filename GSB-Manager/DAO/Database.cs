d31dab5d6e6f627c886656a0b76b7c6c23ce770d
Pablo Schneider <pablo.schneider69@gmail.com>
2026-03-12 11:14:35 +0100

    maj doc

--- GSB-Manager/DAO/Database.cs
+++ GSB-Manager/DAO/Database.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using MySql.Data.MySqlClient;
 
 namespace GSB_Manager.DAO
@@ -9,27 +9,14 @@ namespace GSB_Manager.DAO
 
         public Database()
         {
-#if DEBUG
             const string host = "localhost";
-            const string port = "3311";
+            const string port = "3307";
             const string user = "root";
             const string password = "root";
-#else
-            string host = AWS_HOST;
-            string port = "3306";  
-            string user = AWS_USER;
-            string password = AWS_PASSWORD;
-#endif
 
             myConnectionString =
                 $"server={host};PORT={port};uid={user};pwd={password};database=GSB-Manager;";
         }
-
-#if !DEBUG
-        private const string AWS_HOST = "__AWS_HOST__";
-        private const string AWS_USER = "__AWS_USER__";
-        private const string AWS_PASSWORD = "__AWS_PASSWORD__";
-#endif
 
         public MySqlConnection GetConnection()
         {
